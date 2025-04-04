256fc3c490c8dc33eb6a388db6f955e5bce07ac7
A701\1dam <alemunamo@alu.edu.gva.es>
2025-04-04 16:21:37 +0200

    add ejercicio2
    nostacabaooo DD:

--- src/main/java/org/example/excursion/Estudiante.java
+++ src/main/java/org/example/excursion/Estudiante.java
@@ -1,8 +1,58 @@
 package org.example.excursion;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Estudiante {
     private String apellidos;
     private String nombre;
     private String curso;
     private int edad;
+
+    static Scanner sc = new Scanner(System.in);
+
+    public Estudiante() {
+        crearEstudiante();
+    }
+
+    private void crearEstudiante() {
+        System.out.println("Creando estudiante...\nInserta los apellidos:");
+        this.apellidos = sc.nextLine();
+        System.out.println("Inserta el nombre:");
+        this.nombre = sc.nextLine();
+        System.out.println("Inserta el curso:");
+        this.curso = sc.nextLine();
+        System.out.println("Inserta el edad:");
+        this.edad = sc.nextInt();
+        sc.nextLine();
+        System.out.println(this);
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Estudiante estudiante = (Estudiante) obj;
+        return Objects.equals(curso, estudiante.curso) &&
+                nombre.equals(estudiante.nombre) &&
+                apellidos.equals(estudiante.apellidos);
+    }
+
+    @Override
+    public String toString(){
+        return curso + " " + apellidos + " del curso " + curso + " ";
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
+
--- src/main/java/org/example/excursion/Excursion.java
+++ src/main/java/org/example/excursion/Excursion.java
@@ -1,15 +1,64 @@
 package org.example.excursion;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Excursion {
    private String nombre_actividad;
    private String localidad;
    private double precio;
-   private Coleccion<String> profesores;
-   private Coleccion<Estudiante> listaAsistentes;
+   private List<String> profesores;
+   private List<Estudiante> listaAsistentes;
+
+   static Scanner sc = new Scanner(System.in);
 
    public Excursion(String nombre_actividad, String localidad, double precio) {
        this.nombre_actividad = nombre_actividad;
        this.localidad = localidad;
        this.precio = precio;
+       profesores = new ArrayList<>();
+       listaAsistentes = new ArrayList<>();
    }
+
+   public void addListaAsistentes() {
+       listaAsistentes.add(new Estudiante());
+   }
+
+   public void verAsistentes(){
+       int lista = 0;
+       for (Estudiante estudiante : listaAsistentes){
+           System.out.println("["+lista+"]. " + estudiante.getCurso() + ", " + estudiante.getNombre() + "("+ estudiante.getEdad()+").");
+       }
+   }
+
+   public void eliminarAsistentesPorEdad(int edad){
+       for (Estudiante estudiante : listaAsistentes){
+           if (estudiante.getEdad() < edad){
+               listaAsistentes.remove(estudiante);
+           }
+       }
+   }
+
+    public void insertarProfesor() {
+        System.out.println("Introduce el nombre del profesor/a para la excursión " + nombre_actividad);
+        profesores.add(sc.nextLine());
+        System.out.println("Profesor añadido correctamente a la excursión " + nombre_actividad);
+    }
+
+    public void inscribirAsistente() {
+       addListaAsistentes();
+    }
+
+    public String getNombre_actividad() {
+       return nombre_actividad;
+    }
+
+    public String getLocalidad() {
+       return localidad;
+    }
+
+    public String calcularImporteIngreso() {
+       return "no lo se aún";
+    }
 }
